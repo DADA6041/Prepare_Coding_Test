7859653a078756d38626f16d26cfe8e70fb52e00
DADAKIM <d.ya96v@gmail.com>
2022-10-16 13:55:16 +0900

    :white_check_mark: Solve 29

--- JCB_JS100/js100_01.js
+++ JCB_JS100/js100_01.js
@@ -530,4 +530,24 @@ p t
 
 const gram = prompt('문자를 입력해 주세요.');
 
-for(let i = 0; i < gram.length)
+for(let i = 0; i < gram.length - 1; i++){
+    console.log(gram[i], gram[i + 1]);
+}
+
+// ----------------------------------------------------------
+
+/*
+# 문제29 : 대문자만 지나가세요
+진구는 영어 학원 아르바이트를 하고 있습니다. 반 아이들은 알파벳을 공부하는 학생들인데 오늘은 대문자 쓰기 시험을 봤습니다.
+**알파벳 하나만을 입력하고 그 알파벳이 대문자이면 YES를 아니면 NO를 출력하는 프로그램**을 만들어 주세요.
+*/
+
+const isUpperCase = prompt('문자를 입력해 주세요.');
+
+if (isUpperCase === isUpperCase.toUpperCase()){
+    console.log('YES!');
+} else if(isUpperCase !== isUpperCase.toUpperCase()){
+    console.log('NO!');
+}
+
+// ----------------------------------------------------------
