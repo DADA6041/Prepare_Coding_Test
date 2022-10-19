822b67b29aefb7ff26c475bd08676aa9a334b30c
DADAKIM <d.ya96v@gmail.com>
2022-10-19 16:48:58 +0900

    :white_check_mark: Solve 33

--- JCB_JS100/js100_01.js
+++ JCB_JS100/js100_01.js
@@ -599,3 +599,27 @@ console.log(stringInput.indexOf(stringFind));
 const stringLen = prompt('문자열을 입력해 주세요.');
 
 console.log((stringLen.split(' ')).length);
+
+// ----------------------------------------------------------
+
+/*
+# 문제33 : 거꾸로 출력하기
+한 줄에 여러개의 숫자가 입력되면, 역순으로 그 숫자들을 하나씩 출력하는 프로그램을 작성하시오.
+
+입출력
+
+입력 : 1 2 3 4 5
+출력 : 5 4 3 2 1
+
+입력 : 2 4 6 7 8
+출력 : 8 7 6 4 2
+*/
+
+const inpString = prompt('숫자를 입력해 주세요.');
+
+let tempInp = '';
+
+for (let i = inpString.length - 1; i >= 0; i--){
+    tempInp += inpString[i];
+}
+console.log(tempInp);
