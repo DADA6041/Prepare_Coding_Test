8a993438685357615708369ebf35bafe767b8212
DADAKIM <d.ya96v@gmail.com>
2022-10-20 16:07:16 +0900

    :white_check_mark: Solve 34, 35

--- JCB_JS100/js100_01.js
+++ JCB_JS100/js100_01.js
@@ -623,3 +623,57 @@ for (let i = inpString.length - 1; i >= 0; i--){
     tempInp += inpString[i];
 }
 console.log(tempInp);
+
+// ----------------------------------------------------------
+
+/*
+# 문제34 : sort 구현하기
+민주는 체육부장으로 체육시간이 되면 반 친구들이 제대로 키 순서대로 모였는지를 확인해야 한다. 그런데 요즘 민주는 그것이 너무 번거롭게 느껴져 한 번에 확인하고 싶어한다. 
+
+민주를 위해 **키가 주어지면 순서대로 제대로 섰는지 확인하는 프로그램**을 작성해보자.
+(키는 공백으로 구분하여 입력됩니다.)
+
+입출력
+
+입력 : 176 156 155 165 166 169
+출력 : NO
+
+입력 : 155 156 165 166 169 176
+출력 : YES
+*/
+
+const heightSort = '176 156 155 165 166 169'/* prompt('키를 입력해 주세요.') */
+const sorted = heightSort.split(' ').sort((a, b) => a - b).join(' ');
+
+if (heightSort === sorted) {
+    console.log ('YES');
+} else {
+    console.log('NO');
+}
+
+// ----------------------------------------------------------
+
+/*
+# 문제35 : Factory 함수 사용하기
+2제곱, 3제곱, 4제곱을 할 수 있는 Factory 함수를 만들려고 합니다. 
+<pass>에 코드를 작성하여 two함수를 완성하세요.
+*/
+
+function one(n){
+    function two(param){
+        //pass
+        const square = Math.pow(param, n)
+        return square 
+    }
+    return two;
+}
+
+const a = one(2);
+const b = one(3);
+const c = one(4);
+
+console.log(a(10));
+console.log(b(10));
+console.log(c(10));
+
+// ----------------------------------------------------------
