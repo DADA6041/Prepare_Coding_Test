15d843fe580c85b3730d8ce54e3f90347223f389
DADAKIM <d.ya96v@gmail.com>
2022-11-08 16:40:23 +0900

    :white_check_mark: Solve 53

--- JCB_JS100/js100_02.js
+++ JCB_JS100/js100_02.js
@@ -67,3 +67,29 @@ function quickSort(arr) {
 const array2 = prompt('배열을 입력하세요').split(' ').map(n => parseInt(n, 10));
 
 console.log(quickSort(array2));
+
+// ----------------------------------------------------------
+
+/*
+# 문제53 : 괄호 문자열
+괄호 문자열이란 괄호 기호인 '{', '}', '[', ']', '(', ')' 와 같은 것을 말한다. 그중 괄호의 모양이 바르게 구성된 문자열을 **바른 문자열**, 그렇지 않은 문자열을 **바르지 않은 문자열**이라 부르도록 하자. 
+
+(())와 같은 문자열은 바른 문자열이지만 ()()) 와 같은 문자열은 바르지 않은 문자열이다.
+(해당 문제에서는 소괄호만 판별하지만,  중괄호와 대괄호까지 판별해 보세요.)
+
+**입력으로 주어진 괄호 문자열이 바른 문자열인지 바르지 않은 문자열인지 "YES"와 "NO"로 구분된 문자열을 출력해보자.**
+*/
+
+const parentheses = prompt('괄호를 입력해주세요');
+
+let openCount = 0;
+let closeCount = 0;
+
+for(let i = 0; i < parentheses.length; i++){
+    if(parentheses[i] === '('){
+        openCount ++;
+    } else if(parentheses[i] === ')'){
+        closeCount++;
+    }
+}
+openCount === closeCount ? console.log('Yes') : console.log('No');
