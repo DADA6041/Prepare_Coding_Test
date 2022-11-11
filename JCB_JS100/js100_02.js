c2ebf5a9e035883eea39a0f81647fed553c0728c
DADAKIM <d.ya96v@gmail.com>
2022-11-11 15:24:09 +0900

    :white_check_mark: Solve 56

--- JCB_JS100/js100_02.js
+++ JCB_JS100/js100_02.js
@@ -161,3 +161,41 @@ console.log(route);
 console.log(route.length);
 
 // ----------------------------------------------------------
+
+/*
+# 문제56 : 객체의 함수 응용
+다음의 객체가 주어졌을 때 한국의 면적과 가장 비슷한 국가와 그 차이를 출력하세요.
+
+데이터
+nationWidth = {
+     'korea': 220877,
+     'Rusia': 17098242,
+     'China': 9596961,
+     'France': 543965,
+     'Japan': 377915,
+    'England' : 242900,
+}
+
+출력
+England 22023
+*/
+nationWidth = {
+    'korea': 220877,
+    'Rusia': 17098242,
+    'China': 9596961,
+    'France': 543965,
+    'Japan': 377915,
+    'England': 242900,
+}
+
+let nationArr =  Object.entries(nationWidth);
+
+const nationWidthDiff = nationArr.map(i => [i[0], i[1] - nationArr[0][1]])
+                                .splice(1)
+                                .sort((a, b) => a[1] - b[1]);
+
+let [a, b] = [nationWidthDiff[0][0], nationWidthDiff[0][1]];
+
+console.log(a, b);
+
+// ----------------------------------------------------------
