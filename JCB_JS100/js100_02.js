a6b8f1f344bd55b8b82b51c0e436dbc750e14a42
DADAKIM <d.ya96v@gmail.com>
2022-11-14 10:08:19 +0900

    :white_check_mark: Solve 59

--- JCB_JS100/js100_02.js
+++ JCB_JS100/js100_02.js
@@ -247,3 +247,20 @@ for (let i = 0; i < commaInp.length; i++){
 console.log(commaNum2);
 
 // ----------------------------------------------------------
+
+/*
+# 문제59 : 빈칸채우기
+총 문자열의 길이는 50으로 제한하고 사용자가 문자열을 입력하면 그 문자열을 가운데 정렬을 해주고, 나머지 빈 부분에는 '='을 채워 넣어주세요.
+
+입력
+hi
+
+출력
+========================hi========================
+*/
+
+const centerStr = prompt('문자열을 입력해주세요!');
+
+let dash = 25 + parseInt(centerStr.length / 2);
+
+console.log(centerStr.padStart(dash, '=').padEnd(50, '='));
