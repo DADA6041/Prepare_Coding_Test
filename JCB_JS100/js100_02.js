432ebbff6ab6021c8fa4512feb57b9842aded7c8
DADAKIM <d.ya96v@gmail.com>
2022-11-21 01:20:26 +0900

    :white_check_mark: Solve 66

--- JCB_JS100/js100_02.js
+++ JCB_JS100/js100_02.js
@@ -423,3 +423,55 @@ for (let i = 0; i < arrA.length; i++){
 console.log(arrC);
 
 // ----------------------------------------------------------
+
+/*
+# 문제66 : 블럭탑쌓기
+탑을 쌓기 위해 각 크기별로 준비된 블럭들을 정해진 순서에 맞게 쌓아야 합니다.
+순서에 맞게 쌓지 않으면 무너질 수 있습니다.
+예를 들면 정해진 순서가 BAC 라면 A 다음 C가 쌓아져야 합니다.
+선행으로 쌓아야 하는 블럭이 만족된 경우라면 탑이 무너지지 않습니다.
+
+- B를 쌓지 않아도 A와 C를 쌓을 수 있습니다.
+- B 다음 블럭이 C가 될 수 있습니다.
+
+쌓아져 있는 블럭 탑이 순서에 맞게 쌓아져 있는지 확인하세요.
+
+1. 블럭은 알파벳 대문자로 표기합니다.
+2. 규칙에 없는 블럭이 사용될 수 있습니다.
+3. 중복된 블럭은 존재하지 않습니다.
+
+입력
+탑 = ["ABCDEF", "BCAD", "ADEFQRX", "BEDFG", "EFGHZ"]
+규칙 = "ABD"
+
+출력
+["가능", "불가능", "가능", "가능", "가능"]
+*/
+
+const tower = ["ABCDEF", "BCAD", "ADEFQRX", "BEDFG", "EFGHZ"]
+const rules = "ABD"
+
+function solution(tower, rules) {
+    let answer = [];
+    for (let i of tower){
+        answer.push(stackBlock(i, rules));
+    }
+    return answer;
+}
+
+function stackBlock(i, rules){
+    let temp = rules.indexOf(rules[0]);
+
+    for (let j of i){
+        if(rules.includes(j)){
+            if(temp > rules.indexOf(j)){
+                return '불가능';
+            }
+            temp = rules.indexOf(j);
+        }
+    }
+    return'가능';
+}
+console.log(solution(tower, rules));
+
+// ----------------------------------------------------------
