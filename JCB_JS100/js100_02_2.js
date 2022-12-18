18ffc00d0da8d466111a007e5076783515b60986
DADAKIM <d.ya96v@gmail.com>
2022-12-18 19:23:21 +0900

    :white_check_mark: Solve 87

--- JCB_JS100/js100_02_2.js
+++ JCB_JS100/js100_02_2.js
@@ -293,3 +293,49 @@ function sushiPlate(sushiPoint, sushiDish) {
 console.log(sushiPlate(sushiPoint, sushiDish));
 
 // ----------------------------------------------------------
+
+/*
+# 문제87 : 천하제일 먹기 대회
+천하제일 먹기 대회가 개최되었습니다.
+이 대회는 정해진 시간이 끝난 후 음식을 먹은 그릇 개수를 파악한 후 각 선수들의 등수를 매깁니다. 
+
+1. 같은 이름의 선수는 없습니다.
+2. 접시의 수가 같은 경우는 없습니다.
+
+손오공 야모챠 메지터 비콜로
+70 10 55 40
+
+{'손오공': 1, '메지터': 2, '비콜로': 3, '야모챠': 4}
+
+["홍길동","엄석대","연개소문","김첨지"]
+[2, 1, 10, 0]
+
+{'연개소문': 1, '홍길동': 2, '엄석대': 3, '김첨지': 4}
+*/
+
+const inpName = "손오공 야모챠 메지터 비콜로".split(" ");
+const inpPlate = "70 10 55 40".split(" ").map(i => parseInt(i));
+
+function eatCont(name, plate){
+    let rankSort = plate.map(i => i).sort((a, b) => b - a);
+    let rank = plate.map(j => rankSort.findIndex((item) => item === j) + 1);
+
+    let temp = [];
+    
+    for (let i = 0; i < name.length; i++){
+        let obj ={};
+        obj['name'] = name[i];
+        obj['value'] = rank[i];
+        temp.push(obj);
+    }
+    let tempSort = temp.sort((a, b) => a.value - b.value);
+    let result = {};
+    for(i of tempSort){
+        result[i['name']] = i.value;
+    }
+    return result;
+}
+
+console.log(eatCont(inpName, inpPlate));
+
+// ----------------------------------------------------------
