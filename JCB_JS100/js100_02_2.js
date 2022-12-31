dbc95797c58a0540259e9c36d7403e32b3ca1fee
DADAKIM <d.ya96v@gmail.com>
2022-12-31 23:53:10 +0900

    :white_check_mark: Solve 92

--- JCB_JS100/js100_02_2.js
+++ JCB_JS100/js100_02_2.js
@@ -600,3 +600,75 @@ for (let t of total_score) {
 
 console.log(totalAvg); // 2. 반평균
 console.log(parseInt(totalAvg.reduce((a, b) => a + b) / 7)); // 4. 전체 평균
+
+// ----------------------------------------------------------
+
+/*
+# 문제92 : 키보드 고장
+
+P 회사의 회계를 처리하던 은정은 커피를 마시다가 키보드에 커피를 쏟고 말았습니다.
+휴지로 닦고 말려보았지만 숫자 3, 4, 6이 도통 눌리지 않습니다.
+10분 뒤, 모든 직원들에게 월급을 입금해 주어야 합니다.
+여유 키보드는 없으며, 프로그래밍을 매우 잘하고, 모든 작업을 수작업으로 하고 있습니다.
+
+이에 눌리지 않는 키보드를 누르지 않고 월급 입금을 두 번에 나눠주고 싶습니다.
+
+1. 직원은 2000명이며, 3초 이내 수행을 해야합니다.
+2. 입력값의 형식은 csv파일형식이며 이과장 '3,000,000', 'S은행', '100-0000-0000-000' 형식으로 주어집니다.
+3. 출력값의 형식은 csv파일형식이며 이과장 '1,500,000', '1,500,000', 'S은행', '100-0000-0000-000' 입니다. 또는 '1,000,000', '2,000,000', 'S은행', '100-0000-0000-000' 도 괜찮습니다.
+
+이대표,'333,356,766','S은행','100-0000-0000-001'
+최차장,'5,000,000','S은행','100-0000-0000-002'
+이과장,'3,200,000','S은행','100-0000-0000-003'
+홍팀장,'3,300,000','S은행','100-0000-0000-004'
+이대리,'5,300,000','S은행','100-0000-0000-005'
+*/
+
+const bankInp = `이대표,'333,356,766','S은행','100-0000-0000-001'
+최차장,'5,000,000','S은행','100-0000-0000-002'
+이과장,'3,200,000','S은행','100-0000-0000-003'
+홍팀장,'3,300,000','S은행','100-0000-0000-004'
+이대리,'5,300,000','S은행','100-0000-0000-005'`
+
+let separateInp = bankInp.split('\n');
+let numVal = [];
+
+for (let i of separateInp){
+  let j = i.split(',');
+  let k = j.slice(1, j.length-2);
+  numVal.push(k.join(''))
+}
+
+console.log(numVal);
+let payOne = '';
+let payTow = '';
+let monthlyPayResult = [];
+for (let monthlyPay of numVal){
+  console.log(monthlyPay)
+  for (let separatePay of monthlyPay){
+    console.log(separatePay)
+    if (separatePay != '\''){
+      if (separatePay == 3){
+        payOne += '1';
+        payTow+= '2';
+      } else if (separatePay == 4){
+        payOne += '2';
+        payTow+= '2';
+      } else if (separatePay == 6){
+        payOne += '1';
+        payTow+= '5';
+      } else {
+        payOne += separatePay;
+        payTow+= '0';
+      }
+    }
+  }
+  console.log(payOne);
+  console.log(payTow);
+  monthlyPayResult.push([parseInt(payOne, 10), parseInt(payTow, 10)]);
+  payOne = '';
+  payTow = '';
+}
+console.log(monthlyPayResult);
+
+// ----------------------------------------------------------
