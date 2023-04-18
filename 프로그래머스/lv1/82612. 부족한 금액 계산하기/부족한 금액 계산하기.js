4b38b58249c93ffc3dd9745021320cf447bb52ce
DADA <93987174+DADA6041@users.noreply.github.com>
2023-04-18 14:27:06 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,9 @@
+function solution(price, money, count) {
+    let answer;
+    let total = 0;
+    for(let i = 1; i <= count; i++){
+        total += i * price;
+    }
+    total  > money ? answer = total - money : answer = 0;
+    return answer;
+}
